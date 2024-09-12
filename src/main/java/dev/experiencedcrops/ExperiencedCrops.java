b6f490f29760f44f073dded7366ce8ac9d3da3c5
stlouisn <stlouisn@hotmail.com>
2024-09-12 15:02:33 -0300

    Update ExperiencedCrops.java

--- src/main/java/dev/experiencedcrops/ExperiencedCrops.java
+++ src/main/java/dev/experiencedcrops/ExperiencedCrops.java
@@ -1,17 +1,29 @@
 package dev.experiencedcrops;
 
 import dev.experiencedcrops.config.ModConfig;
-
-import net.fabricmc.api.ModInitializer;
+import dev.experiencedcrops.config.ModConfigData;
+import dev.experiencedcrops.utils.ModConstants;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public final class ExperiencedCrops implements ModInitializer {
+@SuppressWarnings("unused")
+@Mod(ModConstants.MOD_ID)
+public final class ExperiencedCrops {
 
-  @SuppressWarnings("unused")
   public static final Logger LOGGER = LoggerFactory.getLogger(Constants.MOD_ID);
 
-  public void onInitialize() {
-    ModConfig.init();
+  public ModName(IEventBus modBus) {
+    modBus.addListener(this::commonSetup);
+    ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> (client, parent) -> AutoConfig.getConfigScreen(ModConfigData.class, parent).get());
+  }
+
+  private void commonSetup(FMLCommonSetupEvent event) {
+    event.enqueueWork(ModConfig::init);
   }
 }
